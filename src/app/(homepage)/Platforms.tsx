b922352ababdb007f8d9847908e9fa3ddd83c360
Ian O'Brien <ian@ianobrien.dev>
2025-05-12 12:10:56 -0400

    work

--- src/app/(homepage)/Platforms.tsx
+++ src/app/(homepage)/Platforms.tsx
@@ -3,6 +3,7 @@
 import { useRef } from 'react';
 import { motion, useScroll, MotionValue } from 'motion/react';
 import Spinner from './components/Spinner';
+import { Heading } from '@/components/typography';
 
 const Platforms = () => {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -14,29 +15,34 @@ const Platforms = () => {
   // We might need useTransform later if we want to animate based on scrollYProgress
 
   return (
-    <div ref={containerRef} className="relative h-[500vh]"> {/* Ensure this is the scrollable container */}
-      <motion.div 
-        style={{ 
-          position: 'sticky', 
-          top: 0, // Stick to the top of the viewport
-          // We can adjust 'top' if there's a header/navbar, e.g., top: 'var(--header-height)'
-        }}
-        className="h-screen" // The sticky element itself should probably have a defined height, e.g., screen height
-      >
-        <div className="grid grid-cols-2 h-full">
-          <div>sdfg</div>
-          <div>
-            <Spinner parentScrollYProgress={scrollYProgress} />
-          </div>
-        </div>
-      </motion.div>
-
-      {/* This div is outside the sticky element, but within the scrollable container */}
-      {/* It will appear after the 400vh scroll */}
-      <div className="w-full absolute bottom-0">
-        <img src="/bgShape.svg" className="w-full" />
+    <>
+      <div className="relative -bottom-[50vh] max-w-5xl mx-auto">
+        <Heading as="h2" className="text-center">Focused on building exceptional <span className="text-primary">Loyalty experiences</span></Heading>
       </div>
-    </div>
+      <div ref={containerRef} className="relative h-[500vh]"> {/* Ensure this is the scrollable container */}
+        <motion.div 
+          style={{ 
+            position: 'sticky', 
+            top: 0, // Stick to the top of the viewport
+            // We can adjust 'top' if there's a header/navbar, e.g., top: 'var(--header-height)'
+          }}
+          className="h-screen" // The sticky element itself should probably have a defined height, e.g., screen height
+        >
+          <div className="grid grid-cols-2 h-full">
+            <div>sdfg</div>
+            <div>
+              <Spinner parentScrollYProgress={scrollYProgress} />
+            </div>
+          </div>
+        </motion.div>
+
+        {/* This div is outside the sticky element, but within the scrollable container */}
+        {/* It will appear after the 400vh scroll */}
+        <div className="w-full absolute bottom-0">
+          <img src="/bgShape.svg" className="w-full" />
+        </div>
+      </div>
+    </>
   );
 };
 
