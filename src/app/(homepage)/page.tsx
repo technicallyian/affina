c6e099f99d9b141714b967ac4aeb887df0b5b231
Ian O'Brien <ian@ianobrien.dev>
2025-05-21 09:25:39 -0400

    client bs

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -1,11 +1,12 @@
 //import { Section } from '@/components/Section';
-//import { Hero } from './Hero';
+import { Hero } from './Hero';
 //import Platforms from './Platforms';
 import Crowdplay from './Crowdplay';
 import Rethink from './Rethink';
 import WhyAffina from './WhyAffina';
 import Testimonials from './Testimonials';
 import Loyalty from './Loyalty';
+import { Section } from '@/components/Section';
 // Remove direct dynamic imports and original static imports for ThreeD and LogoSlider
 // import dynamic from 'next/dynamic';
 // const ThreeD = dynamic(() => import('./ThreeD'), { ssr: false });
@@ -17,11 +18,11 @@ import Loyalty from './Loyalty';
 export default function Home() {
   return (
     <>
-      {/*<Section className="container mx-auto">
+      <Section className="container mx-auto">
         <div className="absolute top-0 transform left-1/2 -translate-x-1/2 -translate-y-[80%] w-96 h-96 bg-primary/30 rounded-full blur-3xl opacity-99 z-0"></div>
         <Hero />
       </Section>
-      <Platforms />*/} 
+      {/*<Platforms />*/} 
       <Crowdplay /> 
       <Rethink />
       <WhyAffina />
