b3e67bd5aecce53db919638758afccf784a1ccd3
Ian O'Brien <ian@ianobrien.dev>
2025-05-21 09:14:58 -0400

    client bs

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -17,10 +17,10 @@ import Loyalty from './Loyalty';
 export default function Home() {
   return (
     <>
-      <Section className="container mx-auto">
+      {/*<Section className="container mx-auto">
         <div className="absolute top-0 transform left-1/2 -translate-x-1/2 -translate-y-[80%] w-96 h-96 bg-primary/30 rounded-full blur-3xl opacity-99 z-0"></div>
         <Hero />
-      </Section>
+      </Section>*/}
       <Platforms /> 
       <Crowdplay /> 
       <Rethink />
